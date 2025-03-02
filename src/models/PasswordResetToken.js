1fabedb5ac90ccb1b6f51a343052642e66c0349b
Mauricio Duran Torres <mauriciodurant@gmail.com>
2025-03-02 15:56:11 -0300

    add model PasswordResetToken and migration

---
+++ src/models/PasswordResetToken.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes, UUIDV4 } = require('sequelize');
+const sequelize = require('../config/db').sequelize;
+const User = require('./User');
+const moment = require('moment-timezone');
+
+class PasswordResetToken extends Model {}
+
+PasswordResetToken.init({
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: UUIDV4,
+    primaryKey: true,
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: { model: User, key: 'id' },
+  },
+  token: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  expiresAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    get() {
+      return moment(this.getDataValue('expiresAt')).tz('America/Santiago').format();
+    },
+  },
+}, {
+  sequelize,
+  modelName: 'PasswordResetToken',
+  tableName: 'password_reset_tokens',
+  timestamps: true,
+});
+
+PasswordResetToken.belongsTo(User, { foreignKey: 'userId' });
+
+module.exports = PasswordResetToken;
